b9696df141d47b53bb3cd706191a0a15d9d24ef9
Levi <lhalvorson34@gmail.com>
2021-06-22 21:51:35 -0600

    added cart, payment, shipping, orders

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,27 @@ import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
-
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 const App = () => {
   return (
     <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/" exact component={HomeScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/payment" component={PaymentScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" exact component={HomeScreen} />
         </Container>
       </main>
       <Footer />

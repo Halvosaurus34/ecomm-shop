a16b54285b01ba4dd597e64fe759198e380bc84d
Levi <lhalvorson34@gmail.com>
2021-07-02 14:19:40 -0600

    fix spacing on buttons

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -51,7 +51,7 @@ const PaymentScreen = ({ history }) => {
           </Col>
         </Form.Group>
 
-        <Button type='submit' variant='primary'>
+        <Button type='submit' className="buttonMarginTop" variant='primary'>
           Continue
         </Button>
       </Form>
